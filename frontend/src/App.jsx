5da83632f00115715b02e4319fe4b90b80ac5e80
aforAp <satheeshsubbu1612@gmail.com>
2026-06-27 17:42:22 +0530

    clerk and theme context files are added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,21 @@
-import { Show, SignInButton, SignUpButton, UserButton } from "@clerk/react";
-import "./App.css";
-
+import "./index.css";
+import {WallpaperProvider} from "./context/wallPaperContext";
+import {ThemeProvider} from "./context/ThemeContext";
+import { Navigate, Route, Routes } from "react-router";
+import ChatPage from "./pages/ChatPage";
+import AuthPage from "./pages/AuthPage";
+import { useAuth } from "@clerk/react";
 function App() {
+  const {isSignedIn, isLoaded} = useAuth();
   return (
-    <div>
-      <h1>My App</h1>
-      <header>
-        <Show when="signed-out">
-          <SignInButton mode="modal" />
-          <SignUpButton mode="modal" />
-        </Show>
-        <Show when="signed-in">
-          <UserButton />
-        </Show>
-      </header>
-    </div>
+    <ThemeProvider>
+      <WallpaperProvider>
+      <Routes>
+        <Route path="/" element={isSignedIn ? <ChatPage />: <Navigate to={"/auth"} replace/>} />
+        <Route path="/auth" element={!isSignedIn ? <AuthPage /> : <Navigate to={"/chat"} replace />} />
+      </Routes>
+      </WallpaperProvider>
+    </ThemeProvider>
   );
 }
 
